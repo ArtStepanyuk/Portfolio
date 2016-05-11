7275487c9db842459512ec7c48c1d105d7e07779
Art Step <Art Step>
2016-05-11 21:17:48 +0300

    buttons fix text change ToDo's

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -3,8 +3,8 @@
 class NavbarController {
   //start-non-standard
   menu = [{
-    'title': 'Home',
-    'state': 'main'
+    // 'title': 'Home',
+    // 'state': 'main'
   }];
 
   isCollapsed = true;
