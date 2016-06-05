a54ef65d427a5cd88588d97a537ad7da4d5d7ae4
Arthur Stepanyuk <lmasamunel1988@gmail.com>
2016-06-06 02:59:40 +0300

    popovers

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,46 +2,71 @@
 
 (function() {
 
-class MainController {
+    class MainController {
 
-  constructor($http, $scope) {
-    this.$http = $http;
-    this.awesomeThings = [];
-    this.slides = [
-    {
-      image: 'http://moduscreate.com/wp-content/uploads/2014/03/react-opti.png'
-    },
-    {
-      image: 'http://chyngyz.github.io/super-heroic-angular/img/angularjs.png'
+        constructor($http) {
+            this.$http = $http;
+            this.awesomeThings = [];
+            this.myInterval = 3000;
+            // ToDo: move and fill up popover somewhere away
+            this.slides = [{
+                image: 'http://moduscreate.com/wp-content/uploads/2014/03/react-opti.png'
+            }, {
+                image: 'http://chyngyz.github.io/super-heroic-angular/img/angularjs.png'
+            }];
+            this.popoverRetina = {
+                content: 'lorem',
+                title: 'title'
+            };
+            this.popoverSpeed = {
+                content: 'lorem',
+                title: 'title'
+            };
+            this.popoverSupport = {
+                content: 'lorem',
+                title: 'title'
+            };
+            this.popoverSocial = {
+                content: 'lorem',
+                title: 'title'
+            };
+            this.popoverCloud = {
+                content: 'lorem',
+                title: 'title'
+            };
+            this.popoverSpeed = {
+                content: 'lorem',
+                title: 'title'
+            };
+        }
+
+        // use this refference for CRUD
+
+        $onInit() {
+            this.$http.get('/api/things').then(response => {
+                this.awesomeThings = response.data;
+            });
+        }
+
+        addThing() {
+            if (this.newThing) {
+                this.$http.post('/api/things', {
+                    name: this.newThing
+                });
+                this.newThing = '';
+            }
+        }
+
+        deleteThing(thing) {
+            this.$http.delete('/api/things/' + thing._id);
+        }
+
     }
-  ];
-  }
 
-  $onInit() {
-    this.$http.get('/api/things').then(response => {
-      this.awesomeThings = response.data;
-    });
-  }
-
-  addThing() {
-    if (this.newThing) {
-      this.$http.post('/api/things', { name: this.newThing });
-      this.newThing = '';
-    }
-  }
-
-  deleteThing(thing) {
-    this.$http.delete('/api/things/' + thing._id);
-  }
-
-}
-
-  var myInterval = 3000;
-
-angular.module('portfolioApp')
-  .component('main', {
-    templateUrl: 'app/main/main.html',
-    controller: MainController
-  });
+    angular.module('portfolioApp')
+        .component('main', {
+            templateUrl: 'app/main/main.html',
+            controller: MainController
+        });
 
 })();
