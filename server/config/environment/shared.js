03372d63c071f47d88fa48877e10eeeac5807f6d
Arthur Stepanyuk <lmasamunel1988@gmail.com>
2016-06-15 22:46:06 +0300

    Type fixes

--- server/config/environment/shared.js
+++ server/config/environment/shared.js
@@ -13,11 +13,11 @@ exports = module.exports = {
         title: 'Make it Fast'
     },
     popoverSupport: {
-        content: 'The protection of your data and privacy is imperative to me when building your application. Making your information safe and protecting your sleep',
+        content: 'The protection of your data and privacy is imperative to me when building your application. Making your information safe and protecting it while you sleep',
         title: 'Security'
     },
     popoverShop: {
-        content: 'Tired of paying too much? Tired of hiring 5 people to do what one can? You will be pleasantly surprised by my rate and speed',
+        content: 'Tired of paying too much? Tired of hiring 5 people to do what ONE can? You will be pleasantly surprised with my rate and speed.',
         title: 'Prices'
     },
     popoverSocial: {
@@ -25,24 +25,24 @@ exports = module.exports = {
         title: 'Make it Social'
     },
     popoverCloud: {
-        content: 'Whether it is finished product or need for testing environment, I can give you ways of accessing the product on heroku or any other option',
+        content: 'Whether it is a finished product or need for testing environment, I can give you ways of accessing the product on heroku or any other option',
         title: 'Deployment'
     },
     // Feed back
     feedback: [{
     	id : 1,
-    	name: 'Abigail Willson',
+    	name: 'Abigail Wilson',
         image: "assets/images/team-member-1.jpg",
         text: "Dear Arthur Thank you for your help with the  project. It has been great working with you on this occasion. I hope to have the opportunity of working with you again in the near future."
     }, {
     	id : 2,
     	name: 'Patrick Green',
         image: "assets/images/team-member-2.jpg",
-        text: "Arthur Thank you for for helping us out. You are easy to work with, responsible and always thinking about project.I hope we will have chance to work together again. Thanks you again"
+        text: "Arthur Thank you for for helping us out. You are easy to work with, responsible and always thinking about the project. I hope we will have a chance to work together again. Thank you again!"
     }, {
     	id : 3,
-    	name: 'Katrin Johansen',
+    	name: 'Katrina Johansen',
         image: "assets/images/team-member-3.jpg",
-        text: "I want to express my deep appreciation for the help and support you have offered me on my recent project. Without your help, I would not have been able to complete the project."
+        text: "I want to express my deep appreciation for the help and support you have offered me on my recent project. Without your help, I would have not been able to complete the project."
     }]
 };
