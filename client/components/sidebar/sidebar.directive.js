b251512e6280776387b45ac4f4e79e32bc2b61fb
Arthur Stepanyuk <artstepsoftserve@gmail.com>
2016-06-18 17:29:20 +0300

    more styles

--- client/components/sidebar/sidebar.directive.js
+++ client/components/sidebar/sidebar.directive.js
@@ -4,6 +4,9 @@ angular.module('portfolioApp')
   .directive('sideBar', function() {
     return {
       templateUrl: 'components/sidebar/sidebar.html',
-      restrict: 'E'
+      restrict: 'E',
+      controller: function ($window) {
+        $window.scrollTo(0, 0);
+      }
     };
   });
